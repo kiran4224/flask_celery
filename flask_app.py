46cf79953f09249f7ca047aed779a712b94c6d56
Sai KIran <saikiran445445@gmail.com>
2023-11-09 14:30:45 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
-from celery_task import celery_app, logging, my_celery_task
+from celery_task import celery_app, call_tasks
 import os
 import threading
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to flask application"
 
 @app.route('/add', methods=['POST'])
 def add_numbers():
@@ -12,9 +16,10 @@ def add_numbers():
         if request.method == 'POST':
             x = int(request.form['x'])
             y = int(request.form['y'])
-            result = my_celery_task.delay(x, y)
-            worker_hostname = result.backend.get_task_meta(result.id)
-            print(f"Task {result.id} {str(result)} submitted with inputs x={x}, y={y}")
+            result = call_tasks(x,y)
+            # result = my_celery_task.delay(x, y)
+            # worker_hostname = result.backend.get_task_meta(result.id)
+            # print(f"Task {result.id} {str(result)} submitted with inputs x={x}, y={y}")
             return jsonify({"message": "Task submitted", "task_id": result.id}), 202
     except Exception as exc:
         import traceback
@@ -24,9 +29,11 @@ def add_numbers():
 @app.route('/task',methods=["POST"])
 def task_result():
     task_id = str(request.form['task_id'])
-    result = celery_app.AsyncResult(task_id)
-    print("result",result)
-    return jsonify(result)
+    from celery.result import AsyncResult
+    result = AsyncResult(task_id)
+    # result.ready()
+    print("result",result, task_id)
+    return str(result)
 
 if __name__ == '__main__':
     app.run()
